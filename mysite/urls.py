c22327a0e95b4f17ee118b5b1e4f4727b7839c7a
MungaiKeren <wambukeren@gmail.com>
2019-10-20 16:38:25 +0300

    This commit tries to get a single page for the images.. got buggy

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.Registration, name='register')
+    path('register/', views.Registration, name='register'),
+    path('images/(\d+)', views.single_page, name='single_page')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .forms import *
 from .models import Projects
 
@@ -24,3 +24,11 @@ def Registration(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {"form": form})
+
+
+def single_page(request, project_id):
+    try:
+        project = Projects.objects.get(id=project_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, 'project.html', {"project": project})
