b65850b0c3ea2b73f3c536902ce86ebe4d5b9902
MungaiKeren <wambukeren@gmail.com>
2019-10-20 08:20:46 +0300

    This commit makes models profile and image profile

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pic = models.ImageField(upload_to='profile_photo/', blank=True, default='profile_photo/sea_side.jpeg')
+    bio = models.CharField(max_length=265, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='pic_folder')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default='1')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+
+    def __str__(self):
+        return f'{self.profile.user.username}'
+
+    class Meta:
+        db_table = 'images'
+        ordering = ['-post_date']
