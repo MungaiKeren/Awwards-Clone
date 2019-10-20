c893d8e3fe3fd926b9374f0474b3767ee21633d4
MungaiKeren <wambukeren@gmail.com>
2019-10-20 19:52:23 +0300

    This commit adds a route for posting new projects

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.Registration, name='register'),
     path('profile/', views.Profile, name='profile'),
+    path('new_site/', views.post_project, name='new_site'),
     path('images/(\d+)', views.single_page, name='single_page')
 ]
 if settings.DEBUG:
