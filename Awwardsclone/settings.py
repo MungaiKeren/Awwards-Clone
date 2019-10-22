03fadd11cab7c576793aa2b06c55f25bf6e071c6
MungaiKeren <wambukeren@gmail.com>
2019-10-22 08:28:08 +0300

    This commit tries to authenticate the users who can only add projects to database

--- Awwardsclone/settings.py
+++ Awwardsclone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register('projects', views.ProjectView)
@@ -14,7 +15,8 @@ urlpatterns = [
     path('new_site/', views.post_project, name='new_site'),
     path('project/<int:project_id>', views.get_project, name='project'),
     path('search/', views.search_results, name='search'),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api_token_auth/', obtain_auth_token)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
